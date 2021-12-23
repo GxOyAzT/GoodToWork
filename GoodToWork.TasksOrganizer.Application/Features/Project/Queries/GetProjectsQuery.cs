3f17b77f3c8cc4b6c976571a37244d5e4f63d39b
Jakub Koza <jakub.koza13@gmail.com>
2021-12-23 21:16:52 +0100

    add get projects related to user

---
+++ GoodToWork.TasksOrganizer.Application/Features/Project/Queries/GetProjectsQuery.cs
@@ -0,0 +1,6 @@
+﻿using GoodToWork.TasksOrganizer.Domain.Entities;
+using MediatR;
+
+namespace GoodToWork.TasksOrganizer.Application.Features.Project.Queries;
+
+public sealed record GetProjectsQuery(Guid SenderId) : IRequest<List<ProjectEntity>>;
---
+++ GoodToWork.TasksOrganizer.Infrastructure/Features/Project/Queries/GetProjectsHandler.cs
@@ -0,0 +1,26 @@
+﻿using GoodToWork.TasksOrganizer.Application.Features.Project.Queries;
+using GoodToWork.TasksOrganizer.Domain.Entities;
+using GoodToWork.TasksOrganizer.Infrastructure.Persistance.Context;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoodToWork.TasksOrganizer.Infrastructure.Features.Project.Queries;
+
+internal class GetProjectsHandler : IRequestHandler<GetProjectsQuery, List<ProjectEntity>>
+{
+    private readonly AppDbContext _appDbContext;
+
+    public GetProjectsHandler(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
+    public async Task<List<ProjectEntity>> Handle(GetProjectsQuery request, CancellationToken cancellationToken)
+    {
+        return await _appDbContext.ProjectUsers
+            .Include(e => e.Project)
+            .Where(e => e.UserId == request.SenderId)
+            .Select(e => e.Project)
+            .ToListAsync();
+    }
+}
