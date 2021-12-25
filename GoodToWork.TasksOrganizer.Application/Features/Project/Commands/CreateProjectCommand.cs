092fa90b4505063d5faa2c7b798beecc3b1437f3
Jakub Koza <jakub.koza13@gmail.com>
2021-12-25 17:06:07 +0100

    add BaseSenderIdRequest with one SenderId, modify existing commands and queries to derive from BaseSenderIdRequest

--- GoodToWork.TasksOrganizer.Application/Features/Project/Commands/CreateProjectCommand.cs
+++ GoodToWork.TasksOrganizer.Application/Features/Project/Commands/CreateProjectCommand.cs
@@ -1,6 +1,7 @@
-﻿using GoodToWork.TasksOrganizer.Domain.Entities;
+﻿using GoodToWork.TasksOrganizer.Application.Features.Shared;
+using GoodToWork.TasksOrganizer.Domain.Entities;
 using MediatR;
 
 namespace GoodToWork.TasksOrganizer.Application.Features.Project.Commands;
 
-public record CreateProjectCommand(string Name, string Description, Guid SenderId) : IRequest<ProjectEntity>;
+public record CreateProjectCommand(string Name, string Description, Guid SenderId) : BaseSenderIdRequest(SenderId), IRequest<ProjectEntity>;
