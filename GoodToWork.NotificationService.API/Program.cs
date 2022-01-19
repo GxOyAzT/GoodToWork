6392291e73e417a79e24bb26394c62af0423c3e2
Jakub Koza <jakub.koza13@gmail.com>
2022-01-19 17:59:35 +0100

    fix message broker v1

--- GoodToWork.NotificationService.API/Program.cs
+++ GoodToWork.NotificationService.API/Program.cs
@@ -1,7 +1,6 @@
 using GoodToWork.NotificationService.API.HostedServices;
 using GoodToWork.NotificationService.Application;
 using GoodToWork.NotificationService.Application.Configuration;
-using GoodToWork.NotificationService.Application.Features.Email.Commands;
 using GoodToWork.NotificationService.Infrastructure.Emailer.Configuration;
 using GoodToWork.NotificationService.Infrastructure.Persistance.Configuration;
 using GoodToWork.Shared.MessageBroker.DTOs.Email;
@@ -31,8 +30,6 @@ builder.Services.AddMessageBroker(c =>
     c.RegisterConnectionUri(builder.Configuration["RabbitMqConfiguration:ConnectionUri"]);
     c.RegisterListener<EmailCreatedEvent>();
 });
-
-builder.Services.AddTransient<ToDeleteCommand>();
 
 builder.Services.AddHostedService<SendWaitingEmailsService>();
 
