7f48fa5231f0b7ea5179f38a72ed337fbe5b9069
Jakub Koza <jakub.koza13@gmail.com>
2022-01-22 15:24:13 +0100

    add application layer configuration

---
+++ GoodToWork.TasksOrganizer.Application/Configuration/ApplicationConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using GoodToWork.TasksOrganizer.Application.Features.CurrentDateTime;
+using GoodToWork.TasksOrganizer.Application.Features.CurrentDateTime.Interface;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace GoodToWork.TasksOrganizer.Application.Configuration;
+
+public static class ApplicationConfiguration
+{
+    public static IServiceCollection ConfigureInfrastructureLayer(this IServiceCollection services)
+    {
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+
+        services.AddTransient<ICurrentDateTime, GetCurrentDateTime>();
+
+        return services;
+    }
+}
