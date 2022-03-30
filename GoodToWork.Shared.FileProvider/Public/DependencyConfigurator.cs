5c5dee0c918d03e8c0f8f74a1dcf4f38a9f664dd
Jakub Koza <jakub.koza13@gmail.com>
2022-03-31 00:22:04 +0200

    file provider v1.0

---
+++ GoodToWork.Shared.FileProvider/Public/DependencyConfigurator.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace GoodToWork.Shared.FileProvider;
+
+public static class DependencyConfigurator
+{
+    public static IServiceCollection Configure(this IServiceCollection services, Action<Configuration> options)
+    {
+        var config = new Configuration();
+        options(config);
+        services.AddSingleton(config);
+
+        services.AddScoped<IFileProvider, Implementations.FileProvider>();
+
+        return services;
+    }
+}
+
+public class Configuration
+{
+    public string PublicKey { get; set; }
+    public string PrivateKey { get; set; }
+    public string UrlEndPoint { get; set; }
+}
---
+++ GoodToWork.Shared.FileProvider/Utilities/ServerImagekitFactory.cs
@@ -0,0 +1,9 @@
+﻿using Imagekit;
+
+namespace GoodToWork.Shared.FileProvider.Utilities;
+
+internal class ServerImagekitFactory
+{
+    public static ServerImagekit Create(Configuration configuration) =>
+        new ServerImagekit(configuration.PublicKey, configuration.PrivateKey, configuration.UrlEndPoint);
+}
